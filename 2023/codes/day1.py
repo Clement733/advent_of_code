dbe5424798f1b81e0b3262c745de5ba60ce80a6e
clement cipriani <ciprianiclement@outlook.com>
2023-12-01 19:16:15 +0900

    Did day 1

--- 2023/codes/day1.py
+++ 2023/codes/day1.py
@@ -2,38 +2,5 @@ import re
 
 text = open('../inputs/day1')
 
-n = []
-
-
-for line in text:
-    to = []
-    t = []
-    if 'one' in line:
-        line = line.replace('one', '1')
-    if 'two' in line:
-        line = line.replace('two', '2')
-    if 'three' in line:
-        line = line.replace('three', '3')
-    if 'four' in line:
-        line = line.replace('four', '4')
-    if 'five' in line:
-        line = line.replace('five', '5')
-    if 'six' in line:
-        line = line.replace('six', '6')
-    if 'seven' in line:
-        line = line.replace('seven', '7')
-    if 'eight' in line:
-        line = line.replace('eight', '8')
-    if 'nine' in line:
-        line = line.replace('nine', '9')
-    for c in line:
-        if c.isnumeric():
-            t.append(c)
-            to = [t[0], t[-1]]
-            to = ''.join(to)
-            to = int(to)
-
-    n.append(to)
-    print(n)
-print(n[51])
-print(sum(n))
+print(sum((d:=re.findall(r'\d', s)) and int(d[0]+d[-1]) for s in text))
+print(sum(d[0]*10+d[-1]for d in[[n%9+1 for i in range(len(l))for n,w in enumerate("one two three four five six seven eight nine 1 2 3 4 5 6 7 8 9".split())if l[i:i+len(w)]==w]for l in text]))
